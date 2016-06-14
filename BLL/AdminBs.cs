90e65684d93ed6b7dc9501d26ae0844928d1857d
vdkhiem <vdkhiem81@yaoo.com>
2016-06-14 22:03:06 +0800

    Added ApproveOrReject method

--- BLL/AdminBs.cs
+++ BLL/AdminBs.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace BLL
 {
     public class AdminBs : BaseBs
     {
-        
+        public void ApproveOrReject(List<int> ids, string status)
+        {
+            using (TransactionScope trans = new TransactionScope())
+            {
+                try
+                {
+                    foreach (var item in ids)
+                    {
+                        var myUrl = urlBs.GetByID(item);
+                        myUrl.IsApproved = status;
+                        urlBs.Update(myUrl);
+                    }
+
+                    trans.Complete();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+
+            }
+
+        }
     }
 }
